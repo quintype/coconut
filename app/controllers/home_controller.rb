8fb73008318e9b01daa2b0bda26c87cb67c9ce25
Tejas Dinkar <tejas@gja.in>
2016-10-03 04:16:49 +0530

    Basic Section, Tag, Search Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,21 @@ class HomeController < ApplicationController
   def index
     @stories = Story.fetch("top")
   end
+
+  def search
+    @stories = Story.search(q: params[:q]).stories
+  end
+
+  def section
+    section = @config.sections.find {|section| section.slug == params[:section_slug]}
+    if section
+      @stories = Story.fetch("top", "section-id": section.id)
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+  def tag
+    @stories = Story.fetch("top", "tag": params[:tag_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
+STORY_FORMAT = "/:section/:year/:month/:day/:story_slug"
+SECTION_FORMAT = "/section/:section_slug"
+
 Rails.application.routes.draw do
   get "/ping" => "system#ping"
 
-  get "/:section/:year/:month/:day/:story_slug" => "stories#show"
   get "/preview/story" => "stories#preview"
   get "/preview/home" => "home#preview"
 
+  get "/search" => "home#search"
+  get "/topic/:tag_name" => "home#tag"
+
+  get SECTION_FORMAT => "home#section"
+  get STORY_FORMAT => "stories#show"
   root to: "home#index"
 end
