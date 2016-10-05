c280f1d93210ecd7a539b49c0778d193bb54bf6f
tripti rahangdale <imtrapti1410@gmail.com>
2016-10-05 12:15:11 +0530

    moved common imgix options to respective liquid helpers

--- app/assets/javascripts/liquid_helper.js
+++ app/assets/javascripts/liquid_helper.js
@@ -6,6 +6,8 @@ Liquid.registerFilters({
     var image = new quintype.FocusedImage(slug, metadata);
     var aspectRatio = _.chain(aspect_ratio).split("x").map(function(x) { return parseInt(x); }).value();
     var opts = _.chain(arguments).drop(3).chunk(2).fromPairs().value();
+    opts.fmt = 'pjpg'
+    opts.auto = 'compress'
     return "//" + qtConfig.cdnImage + "/" + image.path(aspectRatio, opts);
   }
 });
